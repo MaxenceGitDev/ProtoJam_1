92e59f60278895b650ae1365cf13497641438677
Lisa BELLALI <lisa.bellali92@gmail.com>
2025-02-06 12:04:24 +0100

    View navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 import "./styles/App.css";
 
 function App() {
-	return (
-		<>
-			<Outlet />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import "../styles/navbar.css";
+import Logo from "../assets/images/WildCastle_logo.png";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="logo">
+        <img src={Logo} alt="WildCastle Logo" />
+      </div>
+      <ul>
+        <li>
+          <Link to={"#"}>Villes</Link>
+        </li>
+        <li>
+          <Link to={"#"}>Réservations</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
