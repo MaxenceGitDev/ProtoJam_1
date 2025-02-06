43c21ae1b47545318bf77e1a8ed49f231b86a181
Killian0111 <45.killian.georges@gmail.com>
2025-02-06 15:18:22 +0100

    début page reservation

--- src/main.tsx
+++ src/main.tsx
@@ -11,45 +11,45 @@ import Bals from "./pages/Bals.tsx";
 import Reservation from "./pages/Reservation.tsx";
 
 // import css
-import "./pages/footer/footer.css";
+import "./components/footer/footer.css";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/choice/:city",
-				element: <Choice />,
-			},
-			{
-				path: "/teatime",
-				element: <Teatime />,
-			},
-			{
-				path: "/bals",
-				element: <Bals />,
-			},
-			{
-				path: "/reservation",
-				element: <Reservation />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/choice/:city",
+        element: <Choice />,
+      },
+      {
+        path: "/teatime",
+        element: <Teatime />,
+      },
+      {
+        path: "/bals",
+        element: <Bals />,
+      },
+      {
+        path: "/reservation",
+        element: <Reservation />,
+      },
+    ],
+  },
 ]);
 
 const root = document.getElementById("root");
 
 if (!root) {
-	throw new Error("Erreur");
+  throw new Error("Erreur");
 }
 
 createRoot(root).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,7 +1,17 @@
 export default function Reservation() {
-	return (
-		<>
-			<h1>Reservation</h1>
-		</>
-	);
+  return (
+    <>
+      <div className="reservations">
+        <div className="nav-reservations">
+          <img src="" alt="menu burger choix de langues" />
+          <h1>WildCastle</h1>
+        </div>
+        <div className="buttons">
+          <button type="button">2 couverts</button>
+          <button type="button">Aujourd'hui</button>
+          <button type="button">Réserver</button>
+        </div>
+      </div>
+    </>
+  );
 }
