c37cb9cdce4baeee7da7d426062959ad9be12ea7
Killian0111 <45.killian.georges@gmail.com>
2025-02-07 11:25:48 +0100

    changement droite a gauche

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,14 @@
 export default function Footer() {
-	return (
-		<>
-			<footer className="footer-containers">
-				<img src="" alt="Logo" />
-				<h1>@2025 WCS - Wild Castle</h1>
-				<img src="" alt="Logo-réseaux-sociaux" />
-			</footer>
-		</>
-	);
+  return (
+    <>
+      <footer className="footer-containers">
+        <img
+          src="/src/assets/images/WildCastle_logo.png"
+          alt="WildCastle Logo"
+        />
+        <h1>@2025 WCS - Wild Castle</h1>
+        <img src="" alt="Logo-réseaux-sociaux" />
+      </footer>
+    </>
+  );
 }
